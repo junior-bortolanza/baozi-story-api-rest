1f8db9c9197bd3f3247caa017c1875a4372c9ff4
Junior Bortolanza <gbortolanzajr@gmail.com>
2026-06-28 20:07:02 -0300

    first commit

---
+++ src/main/java/com/BaoziStore/Baozi/Store/BaoziStoreApplication.java
@@ -0,0 +1,13 @@
+package com.BaoziStore.Baozi.Store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaoziStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaoziStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BaoziStore/Baozi/Store/BaoziStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BaoziStore.Baozi.Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaoziStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
